3e7c62ad1d69ac62fef0b5459d54d6a4ad4264d0
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-10 00:40:04 -0500

    add questions to ask the user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the name of your project?', 'How would you describe your project?', 'Installation instructions?', 'Usage information?', 'Contribution guidelines?', 'Testing instructions?','License?', 'Github username?', 'Email address?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
