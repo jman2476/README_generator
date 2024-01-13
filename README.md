d0bbbe1450d1483c39093eea13064d2e50734afa
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-13 13:42:30 -0500

    add questions and functionality to prompt user
    made the questions array an array of objects, and made description,
    installation, testing, and usage to bring up text editor windows to
    input information

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# README_generator
+# README generator
 
--- index.js
+++ index.js
@@ -5,13 +5,30 @@ const genMD = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = require('./utils/questions')
 
+
 // Function to get user input
 function userInput () {
-
+    
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(path, data) { 
+    //write data to the file using fs
+    fs.writeFile(path, data, (err)=> {
+        if (err) return console.log(err)
+
+        // 
+        console.log('Check out your new README file!')
+    })
+}
+
+inquirer.prompt(questions)
+    .then((answersObj) => {
+        const markdownTxt = genMD(answersObj);
+
+        // create the readme file
+        writeToFile('./README.md', markdownTxt)
+    })
 
 // TODO: Create a function to initialize app
 function init() { }
